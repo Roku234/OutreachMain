ff89a12e39f334581dda1b30ae5139c62ca8298f
Bartek Pacia <barpac02@gmail.com>
2018-11-05 14:44:10 +0100

    App is working fine now. Replaced OkHttp with Volley

--- app/src/main/java/me/rohitmukund/outreach/MainActivity.java
+++ app/src/main/java/me/rohitmukund/outreach/MainActivity.java
@@ -1,80 +1,75 @@
 package me.rohitmukund.outreach;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    final String url = "https://api.github.com/orgs/JBossOutreach/repos";
     ArrayList<String> repositories;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ListView listView = findViewById(R.id.list);
-        repositories = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, repositories);
+        repositories = new ArrayList<>();
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, repositories);
         listView.setAdapter(arrayAdapter);
-        OkHttpClient client = new OkHttpClient();
-        final String url = "https://api.github.com/orgs/JBossOutreach/repos";
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        client.newCall(request).enqueue(new Callback() {
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
+        {
             @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
+            public void onResponse(String response)
+            {
+                Log.d("MainActivity", response);
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if(response.isSuccessful()) {
-                    String myResponse = response.body().toString();
+                try
+                {
+                    JSONArray arr = new JSONArray(response);
+                    for (int i = 0; i < arr.length(); i++)
+                    {
+                        JSONObject part = arr.getJSONObject(i);
+                        String repoName = part.getString("name");
 
-                    try {
-                        JSONArray arr = new JSONArray(myResponse);
-                        for(int i = 0; i <arr.length(); i++) {
-                            JSONObject part = arr.getJSONObject(i);
-                            String repoName = part.getString("name");
-
-                            repositories.add(repoName);
-
-
-
-
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                        repositories.add(repoName);
+                        arrayAdapter.notifyDataSetChanged();
                     }
+
+                } catch (JSONException e)
+                {
+                    e.printStackTrace();
                 }
             }
 
+        }, new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                error.printStackTrace();
+            }
         });
-
-
+        requestQueue.add(request);
     }
-
-
-
 }
