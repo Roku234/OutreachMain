1c0cb5cc84979a7e75822672dfa78d7d20e2c882
RohitMTheOne <rohitmukund11@gmail.com>
2018-11-06 19:27:16 -0500

    Updated

---
+++ app/src/main/java/me/rohitmukund/outreach/Information.java
@@ -0,0 +1,18 @@
+package me.rohitmukund.outreach;
+
+public class Information {
+    int counter;
+     String[] urls = {"https://api.github.com/repos/JBossOutreach/blog.jbossoutreach.org/contributors","https://api.github.com/repos/JBossOutreach/archive.jbossoutreach.org/contributors","https://api.github.com/repos/JBossOutreach/lead-management-android/contributors",
+     "https://api.github.com/repos/JBossOutreach/lead-management-server/contributors","https://api.github.com/repos/JBossOutreach/contact-groups-android/contributors","https://api.github.com/repos/JBossOutreach/contact-groups-server/contributors"
+     ,"https://api.github.com/repos/JBossOutreach/jbossoutreach.org/contributors","https://api.github.com/repos/JBossOutreach/ideas-list/contributors","https://api.github.com/repos/JBossOutreach/certificate-generator-server/contributors",
+     "https://api.github.com/repos/JBossOutreach/visiting-card-front/contributors","https://api.github.com/repos/JBossOutreach/certificate-generator-front/contributors","https://api.github.com/repos/JBossOutreach/lead-management-front/contributors",
+     "https://api.github.com/repos/JBossOutreach/codein-mentors-playground/contributors","https://api.github.com/repos/JBossOutreach/gci18.jbossoutreach.org/contributors","https://api.github.com/repos/JBossOutreach/mentors-handbook/contributors",
+     "https://api.github.com/repos/JBossOutreach/visiting-card-android/contributors","https://api.github.com/repos/JBossOutreach/visiting-card-server/contributors","https://api.github.com/repos/JBossOutreach/jboss-logo/contributors"};
+     public void setCounter(int counter) {
+         this.counter = counter;
+     }
+     public int getCounter() {
+         return counter;
+     }
+    }
+
--- app/src/main/java/me/rohitmukund/outreach/MainActivity.java
+++ app/src/main/java/me/rohitmukund/outreach/MainActivity.java
@@ -1,8 +1,11 @@
 package me.rohitmukund.outreach;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,7 +26,11 @@ public class MainActivity extends AppCompatActivity
 {
     final String url = "https://api.github.com/orgs/JBossOutreach/repos";
     ArrayList<String> repositories;
-
+    Information info = new Information();
+    int counter = 0;
+    public int getCounter() {
+        return counter;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,6 +42,111 @@ public class MainActivity extends AppCompatActivity
 
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, repositories);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position) {
+                    case 0:
+                        Intent myintent = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(0);
+                        startActivityForResult(myintent,0);
+                        break;
+                    case 1:
+                        Intent myintent2 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(1);
+                        startActivity(myintent2);
+                        startActivityForResult(myintent2,0);
+                        finish();
+                        startActivity(getIntent());
+                        break;
+                    case 2:
+                        Intent myintent3 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(2);
+                        startActivityForResult(myintent3,0);
+                        break;
+                    /*8case 3:
+                        Intent myintent4 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(3);
+                        startActivityForResult(myintent4,0);
+                        break;
+
+                    case 4:
+                        Intent myintent5 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(4);
+                        startActivityForResult(myintent5,0);
+                        break;
+                    case 5:
+                        Intent myintent6 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(5);
+                        startActivityForResult(myintent6,0);
+                        break;
+                    case 6:
+                        Intent myintent7 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(6);
+                        startActivityForResult(myintent7,0);
+                        break;
+                    case 7:
+                        Intent myintent8 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(7);
+                        startActivityForResult(myintent8,0);
+                        break;
+                    case 8:
+                        Intent myintent9 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(8);
+                        startActivityForResult(myintent9,0);
+                        break;
+                    case 9:
+                        Intent myintent10 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(9);
+                        startActivityForResult(myintent10,0);
+                        break;
+                    case 10:
+                        Intent myintent11 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(10);
+                        startActivityForResult(myintent11,0);
+                        break;
+                    case 11:
+                        Intent myintent12 = new Intent(view.getContext(),Option1.class);
+
+                        info.setCounter(11);
+                        startActivityForResult(myintent12,0);
+                        break;
+                    case 12:
+                        Intent myintent13 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(12);
+                        startActivityForResult(myintent13,0);
+                        break;
+                    case 13:
+                        Intent myintent14 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(13);
+                        startActivityForResult(myintent14,0);
+                        break;
+                    case 14:
+                        Intent myintent15 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(14);
+                        startActivityForResult(myintent15,0);
+                        break;
+                    case 15:
+                        Intent myintent16 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(15);
+                        startActivityForResult(myintent16,0);
+                        break;
+                    case 16:
+                        Intent myintent17 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(16);
+                        startActivityForResult(myintent17,0);
+                        break;
+                    case 17:
+                        Intent myintent18 = new Intent(view.getContext(),Option1.class);
+                        info.setCounter(17);
+                        startActivityForResult(myintent18,0);
+                        break;
+*/
+
+                }
+            }
+        });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>()
@@ -63,6 +175,7 @@ public class MainActivity extends AppCompatActivity
             }
 
         }, new Response.ErrorListener()
+
         {
             @Override
             public void onErrorResponse(VolleyError error)
@@ -72,4 +185,6 @@ public class MainActivity extends AppCompatActivity
         });
         requestQueue.add(request);
     }
+
+
 }
